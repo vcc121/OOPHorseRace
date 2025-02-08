6c2db00c9692fc2a930c7cabb4a5bc363df1af23
vcc121 <vaughn.cox@bsu.edu>
2025-02-08 04:02:26 +0000

    fixing

--- race.h
+++ race.h
@@ -1,9 +1,13 @@
 #ifndef RACE_H_EXISTS
 #define RACE_H_EXISTS
+#include horse.h
+
+const int TRACK_LENGTH = 15;
+const int NUM_HORSES = 5;  
 
 class Race {
 	private:
-		Horse h[5];
+		Horse horses[NUM_HORSES];
 	public:
 		Race();
 		void run();
