24e13b012e1d60c0aec63f4af71a0b14a47628a1
vcc121 <vaughn.cox@bsu.edu>
2025-02-08 03:59:39 +0000

    Fixed some things

--- horse.h
+++ horse.h
@@ -5,10 +5,10 @@ class Horse {
 	private:
 		int position;
 		int trackLength;
-		int id;
+		int index;
 	public:
 		Horse();
-		init(int id, int trackLength);
+		init(int index, int trackLength);
 		void advance();
 		void printLane();
 		bool isWinner ();
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include horse.cpp
+#include race.cpp
+#include horse.h
+#include race.h
+
+int main() {
+    Race race;
+    race.run();
+    return 0;
+}
